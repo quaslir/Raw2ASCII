266ed524354cc607edfa96f1746f875ca383e91c
Karl <karl@mac.wifi.saxion.nl>
2026-03-16 20:44:42 +0100

    sync

--- src/rgb.cpp
+++ src/rgb.cpp
@@ -2,8 +2,15 @@
 #include <cstdio>
 #include <iostream>
 void RGB::printPixel(std::stringstream & ss) const {
+if(alpha == 0) {
+    ss << " ";
+    return;
+}
+
+const static std::string ramp = " .:-=+*#%@";
+char symbol = ramp[alpha * (ramp.size() - 1) / 255];
 char buffer[64];
-std::snprintf(buffer, sizeof(buffer), "\x1b[38;2;%u;%u;%um@\x1b[0m", r, g, b);
+std::snprintf(buffer, sizeof(buffer), "\x1b[38;2;%u;%u;%um%c\x1b[0m", r, g, b, symbol);
 
 ss << buffer;
 }
